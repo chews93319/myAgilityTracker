e895f130e21a07032288adee688d5f126626cb79
chews93319 <chews@oregonstate.edu>
2018-08-13 00:29:22 -0700

    Successful GET and POST of GooglePlus API

--- app/src/main/java/oregonstate/chews/myagilitytracker/APIActivity.java
+++ app/src/main/java/oregonstate/chews/myagilitytracker/APIActivity.java
@@ -8,8 +8,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import net.openid.appauth.AuthState;
@@ -19,15 +23,36 @@ import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.ResponseTypeValues;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import oregonstate.chews.myagilitytracker.R;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class APIActivity extends AppCompatActivity {
 
     private AuthorizationService mAuthorizationService;
     private AuthState mAuthState;
-    //private OkHttpClient mOkHttpClient;
+    private OkHttpClient mOkHttpClient;
+
+    //Create a Log TAG [ref: 1]
+    private static final String TAG = "APIActivity";
 
 
     @Override
@@ -43,12 +68,139 @@ public class APIActivity extends AppCompatActivity {
                     mAuthState.performActionWithFreshTokens(mAuthorizationService, new AuthState.AuthStateAction(){
                         @Override
                         public void execute(@Nullable String accessToken, @Nullable String idToken, @Nullable AuthorizationException e) {
-                            ((TextView)findViewById(R.id.textAccessToken)).setText(accessToken);
+                            if (e == null){
+                                mOkHttpClient = new OkHttpClient();
+                                HttpUrl reqUrl = HttpUrl.parse("https://www.googleapis.com/plusDomains/v1/people/me/activities/user");
+                                Request request = new Request.Builder()
+                                        .url(reqUrl)
+                                        .addHeader("Authorization", "Bearer " + accessToken)
+                                        .build();
+                                mOkHttpClient.newCall(request).enqueue(new Callback() {
+                                    @Override
+                                    public void onFailure(Call call, IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) throws IOException {
+                                        String r = response.body().string();
+                                        Log.d(TAG, r);
+
+                                        try {
+                                            JSONObject j = new JSONObject(r);
+                                            JSONArray items = j.getJSONArray("items");
+
+                                            List<Map<String,String>> posts = new ArrayList<>();
+                                            for(int i = 0; i < items.length(); i++){
+                                                HashMap<String, String> m = new HashMap<>();
+                                                m.put("published", items.getJSONObject(i).getString("published"));
+                                                m.put("title",items.getJSONObject(i).getString("title"));
+                                                if (i < 3){
+                                                    posts.add(m);
+                                                }
+                                            }
+                                            final SimpleAdapter postAdapter = new SimpleAdapter(
+                                                    APIActivity.this,
+                                                    posts,
+                                                    R.layout.google_plus_item,
+                                                    new String[]{"published", "title"},
+                                                    new int[]{R.id.google_plus_item_date_text, R.id.google_plus_item_text});
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    ((ListView)findViewById(R.id.google_post_list)).setAdapter(postAdapter);
+                                                }
+                                            });
+                                        } catch (JSONException e1) {
+                                            e1.printStackTrace();
+                                        }
+                                    }
+                                });
+
+                            }
                         }
                     });
                 } catch (Exception e){
                     e.printStackTrace();
                 }
+            }
+        });
+
+        ((Button)findViewById(R.id.btnPost)).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try{
+                    mAuthState.performActionWithFreshTokens(mAuthorizationService, new AuthState.AuthStateAction(){
+                        @Override
+                        public void execute(@Nullable String accessToken, @Nullable String idToken, @Nullable AuthorizationException e) {
+                            if (e == null){
+                                String postContent = ((EditText)findViewById(R.id.inputPost)).getText().toString();
+                                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+                                mOkHttpClient = new OkHttpClient();
+                                HttpUrl reqUrl = HttpUrl.parse("https://www.googleapis.com/plusDomains/v1/people/me/activities");
+
+                                String msg = ((EditText)findViewById(R.id.inputPost)).getText().toString();
+                                JSONObject myJ = new JSONObject();
+                                JSONObject myMsg = new JSONObject();
+                                JSONObject myAccess = new JSONObject();
+                                JSONArray myItems = new JSONArray();
+                                JSONObject myItem = new JSONObject();
+                                try {
+                                    myMsg.put("content",msg);
+                                    myItem.put("type","domain");
+                                    myItems.put(myItem);
+                                    myAccess.put("items",myItems);
+                                    myAccess.put("domainRestricted",true);
+
+                                    myJ.put("object",myMsg);
+                                    myJ.put("access",myAccess);
+
+                                    Log.d(TAG, myJ.toString());
+                                } catch (JSONException e2) {
+                                    e2.printStackTrace();
+                                }
+
+                                RequestBody mBody = RequestBody.create(JSON, myJ.toString());
+                                Request request = new Request.Builder()
+                                        .url(reqUrl)
+                                        .addHeader("Authorization", "Bearer " + accessToken)
+                                        .post(mBody)
+                                        .build();
+                                mOkHttpClient.newCall(request).enqueue(new Callback() {
+                                    @Override
+                                    public void onFailure(Call call, IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    @Override
+                                    public void onResponse(Call call, Response response) throws IOException {
+                                        String r = response.body().string();
+                                        Log.d(TAG, r);
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+
+
+/*
+                String myPost(String url, String json) throws IOException {
+                    RequestBody body = RequestBody.create(JSON, json);
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .post(body)
+                            .build();
+                    Response response = mOkHttpClient.newCall(request).execute();
+                    return response.body().string();
+                }
+*/
+                ((EditText)findViewById(R.id.inputPost)).setText("");
             }
         });
 
@@ -58,6 +210,12 @@ public class APIActivity extends AppCompatActivity {
     protected void onStart() {
         mAuthState = getOrCreateAuthState();
         super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mAuthorizationService.dispose();
+        super.onDestroy();
     }
 
     //   Function will check if we have a good authorization
